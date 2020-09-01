0afa23b694cc43a5bc9e79db0bf6f211d0a1e7c6
William Hampton <willhamp@umich.edu>
2020-09-01 13:48:48 -0400

    feat: Renamed package

--- examples/gps_reader.py
+++ examples/gps_reader.py
@@ -2,7 +2,7 @@ from ublox_gps import UbloxGps
 import serial
 import numpy as np
 
-from gps_jamming_detector import GPSSpy
+from hoverfly_gpsspy.gps_spy import GPSSpy
 
 
 NUM_SATS = 71
--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ import setuptools
 with open("README.md", 'r') as fh:
     long_description = fh.read()
 
+print(setuptools.find_packages('src'))
+
 setuptools.setup(
-    name="hoverfly-gpsspy",  # Replace with your own username
-    version="0.0.1",
+    name="gpsspy",  # Replace with your own username
+    version="0.0.2",
     author="William Hampton",
     author_email="william.hampton@hoverflytech.com",
     description="An implementation of GPS jamming detection",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/HoverflyHampton/Qwiic_Ublox_Gps_Py",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
