6a4e272d56b546d5350aaa52d5ce09ace3addced
William Hampton <william.hampton@hoverflytech.com>
2021-05-12 17:06:48 -0400

    fix: Minor code cleanup in example

--- examples/gps_raspy_beeper.py
+++ examples/gps_raspy_beeper.py
@@ -2,6 +2,7 @@ from ublox_gps import UbloxGps
 import RPi.GPIO as GPIO 
 import serial
 import numpy as np
+import time
 
 from gpsspy.gps_spy import GPSSpy
 
@@ -12,39 +13,48 @@ gps = UbloxGps(port)
 
 spy = GPSSpy(signal_len=5, num_sats=NUM_SATS)
 
-buzzer = 13
-buzzCount = 0
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
 
 
 def run():
+    jamming_known = False
+    buzzer = 27
+    buzzCount = 0
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(buzzer, GPIO.OUT)
+    GPIO.output(buzzer, GPIO.LOW)
+    buzzDrive = GPIO.PWM(buzzer, 50)
+    with open("/home/pi/hti/logging/gps_jamming/"+timestr+".csv", 'w') as logFile:
+        logFile.write("Time,CNO,Jammed\n")
+        try:
+            print("Listening for UBX Messages.")
+            while True:
+                try:
+                    buzzCount += 1
+                    if buzzCount > 0:
+                        buzzCount = 0
+                        buzzDrive.stop()
+                    sats = gps.satellites()
+                    next_cno = np.zeros(NUM_SATS)
+                    sat_dat = [(s.svId, s.cno) for s in sats.RB]
+                    for val in sat_dat:
+                        next_cno[val[0]] = val[1]
+                    val = spy.step(next_cno)
+                    logFile.write("{},{},{}\n".format(time.time(), val, spy.jammed))
+                    if spy.jammed and not jamming_known:
+                        jamming_known = True
+                        buzzDrive.start(70)
+                    elif not spy.jammed and jamming_known:
+                        jamming_known = False
+                        buzzDrive.stop()
+                except (ValueError, IOError) as err:
+                    print(err)
 
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(buzzer, GPIO.OUT)
-  GPIO.output(buzzer, GPIO.LOW)
-
-  try:
-    print("Listenting for UBX Messages.")
-    while True:
-      try:
-        buzzCount += 1
-        if buzzCount > 0:
-            buzzCount = 0
-            GPIO.output(buzzer, GPIO.HIGH)
-        sats = gps.satellites()
-        next_cno = np.zeros(NUM_SATS)
-        sat_dat = [(s.svId, s.cno) for s in sats.RB]
-        for val in sat_dat:
-            next_cno[val[0]] = val[1]
-        print(spy.step(next_cno))
-        if(spy.jammed):
-            GPIO.output(buzzer, GPIO.HIGH)
-            buzzCount = -10
-      except (ValueError, IOError) as err:
-        print(err)
-
-  finally:
-    port.close()
+        finally:
+            port.close()
+            GPIO.cleanup()
 
 
 if __name__ == '__main__':
