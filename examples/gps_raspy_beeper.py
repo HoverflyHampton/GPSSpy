517c15682430efe902534dbc856c9b5c25cfb278
William Hampton <william.hampton@hoverflytech.com>
2021-05-13 14:36:43 -0400

    fix: Let the buzzer actually sound off when jammed

--- examples/gps_raspy_beeper.py
+++ examples/gps_raspy_beeper.py
@@ -21,40 +21,43 @@ timestr = time.strftime("%Y%m%d-%H%M%S")
 def run():
     jamming_known = False
     buzzer = 27
-    buzzCount = 0
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(buzzer, GPIO.OUT)
     GPIO.output(buzzer, GPIO.LOW)
-    buzzDrive = GPIO.PWM(buzzer, 50)
-    with open("/home/pi/hti/logging/gps_jamming/"+timestr+".csv", 'w') as logFile:
-        logFile.write("Time,CNO,Jammed\n")
-        try:
-            print("Listening for UBX Messages.")
-            while True:
-                try:
-                    buzzCount += 1
-                    if buzzCount > 0:
-                        buzzCount = 0
-                        buzzDrive.stop()
-                    sats = gps.satellites()
-                    next_cno = np.zeros(NUM_SATS)
-                    sat_dat = [(s.svId, s.cno) for s in sats.RB]
-                    for val in sat_dat:
-                        next_cno[val[0]] = val[1]
-                    val = spy.step(next_cno)
-                    logFile.write("{},{},{}\n".format(time.time(), val, spy.jammed))
-                    if spy.jammed and not jamming_known:
-                        jamming_known = True
-                        buzzDrive.start(70)
-                    elif not spy.jammed and jamming_known:
-                        jamming_known = False
-                        buzzDrive.stop()
-                except (ValueError, IOError) as err:
-                    print(err)
+    buzzDrive = GPIO.PWM(buzzer, 2000)
+    # with open("/home/pi/hti/logging/gps_jamming/"+timestr+".csv", 'w') as logFile:
+    # logFile.write("Time,CNO,Jammed\n")
+    try:
+        print("Listening for UBX Messages.")
+        buzzDrive.start(70)
+        time.sleep(1)
+        buzzDrive.stop()
+        time.sleep(1)
+        buzzDrive.start(70)
+        time.sleep(1)
+        buzzDrive.stop()
+        while True:
+            try:
+                sats = gps.satellites()
+                next_cno = np.zeros(NUM_SATS)
+                sat_dat = [(s.svId, s.cno) for s in sats.RB]
+                for val in sat_dat:
+                    next_cno[val[0]] = val[1]
+                val = spy.step(next_cno)
+                print(val, spy.jammed, jamming_known)
+                # logFile.write("{},{},{}\n".format(time.time(), val, spy.jammed))
+                if spy.jammed and not jamming_known:
+                    jamming_known = True
+                    buzzDrive.start(70)
+                elif not spy.jammed and jamming_known:
+                    jamming_known = False
+                    buzzDrive.stop()
+            except (ValueError, IOError, IndexError) as err:
+                print(err)
 
-        finally:
-            port.close()
-            GPIO.cleanup()
+    finally:
+        port.close()
+        GPIO.cleanup()
 
 
 if __name__ == '__main__':
